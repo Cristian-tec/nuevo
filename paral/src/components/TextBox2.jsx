6073a200050afe075c1b80a22f52f8585a877660
Cristian <89481577+Cristian-tec@users.noreply.github.com>
2024-04-15 20:36:19 -0300

    2

--- paral/src/components/TextBox2.jsx
+++ paral/src/components/TextBox2.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import fiesta from "../img/fiesta.svg";
 
 function TextBox2() {
+
+ let ubicacion = "https://www.google.com/maps/dir/-33.2727082,-66.2248175/la+cascada+salon+de+eventos/@-33.26671,-66.2547007,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95d43f82ddbf5d13:0xf5a217669af2d966!2m2!1d-66.2437474!2d-33.2584752?entry=ttu"
+    
+  
   return (
     <>
       <div class="text-box2">
@@ -16,7 +20,9 @@ function TextBox2() {
           Clickeá en el botón de abajo y encontrá las indicaciones para llegar,
           ¡nos vemos!😉
         </h2>
-        <button>CÓMO LLEGAR</button>
+        <a href={ubicacion} target="_blank">
+          <button>CÓMO LLEGAR</button>
+          </a>
       </div>
     </>
   );
