2daafd92ab47f612494037149347e1bcf7e64df2
Cristian <89481577+Cristian-tec@users.noreply.github.com>
2024-09-02 13:37:34 -0300

    fin1

--- paral/src/components/TexBox3.jsx
+++ paral/src/components/TexBox3.jsx
@@ -7,8 +7,10 @@ function TextBox3() {
       <div class="text-box2">
         <h3 class="fiesta">CONFIRMACIÓN DE ASISTENCIA</h3>
         <h2>Cuento con tu presencia. </h2>
-        <h2>¡Confirmá tu asistencia!</h2>
-        <a href="https://wa.link/njhw20" target="_blank"><button>CONFIRMAR ASISTENCIA</button></a>
+        <h2>Confirmá tu asistencia. Tienes tiempo hasta el 10 de noviembre!</h2>
+        <a href="https://wa.link/njhw20" target="_blank">
+          <button>CONFIRMAR ASISTENCIA</button>
+        </a>
       </div>
     </>
   );
--- paral/src/components/TextBox2.jsx
+++ paral/src/components/TextBox2.jsx
@@ -2,10 +2,9 @@ import React from "react";
 import fiesta from "../img/fiesta.svg";
 
 function TextBox2() {
+  let ubicacion =
+    "https://www.google.com/maps/dir/-33.2727082,-66.2248175/la+cascada+salon+de+eventos/@-33.26671,-66.2547007,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95d43f82ddbf5d13:0xf5a217669af2d966!2m2!1d-66.2437474!2d-33.2584752?entry=ttu";
 
- let ubicacion = "https://www.google.com/maps/dir/-33.2727082,-66.2248175/la+cascada+salon+de+eventos/@-33.26671,-66.2547007,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x95d43f82ddbf5d13:0xf5a217669af2d966!2m2!1d-66.2437474!2d-33.2584752?entry=ttu"
-    
-  
   return (
     <>
       <div class="text-box2">
@@ -15,14 +14,17 @@ function TextBox2() {
           ¡Te espero el Sábado 14 de diciembre de 21:00 a 05:00 hs para festejar
           este gran momento de mi vida!{" "}
         </h2>
-        <h2>Salón ... Ya lo verás... {/* Las Chacras, Juana Koslay. */}</h2>
+        <h2>
+          Salón La Cascada. Las Chacras - Juana Koslay.{" "}
+          {/* Las Chacras, Juana Koslay. */}
+        </h2>
         <h2>
           Clickeá en el botón de abajo y encontrá las indicaciones para llegar,
           ¡nos vemos!😉
         </h2>
         <a href={ubicacion} target="_blank">
           <button>CÓMO LLEGAR</button>
-          </a>
+        </a>
       </div>
     </>
   );
