56fc53f4a1dc9a48db3ebcb8ca3b9958e9deba3d
Cristian <89481577+Cristian-tec@users.noreply.github.com>
2024-10-08 18:36:51 -0300

    eleg

--- paral/src/components/TextBox4.jsx
+++ paral/src/components/TextBox4.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import image from '../img/icono-dresscode.svg'
+import image from "../img/icono-dresscode.svg";
 
 function TextBox4() {
-
   return (
     <>
       <div class="text-box4">
-      <img class="imgAnim" src={image} alt="fiesta" />
+        <img class="imgAnim" src={image} alt="fiesta" />
         <h3 class="classh3">DRES CODE</h3>
-        <h2>Elegante Sport</h2>
+        <h2>Elegante</h2>
       </div>
     </>
   );
