4faf20425c4075d1a8af273c7a2fb72b71539964
Cristian <89481577+Cristian-tec@users.noreply.github.com>
2024-11-12 18:07:11 -0300

    Update TexBox3.jsx
    www

--- paral/src/components/TexBox3.jsx
+++ paral/src/components/TexBox3.jsx
@@ -7,7 +7,7 @@ function TextBox3() {
       <div class="text-box2">
         <h3 class="fiesta">CONFIRMACIÓN DE ASISTENCIA</h3>
         <h2>Cuento con tu presencia. </h2>
-        <h2>Confirmá tu asistencia. Tienes tiempo hasta el 10 de noviembre!</h2>
+        <h2>Confirmá tu asistencia. Tienes tiempo hasta el 1 de diciembre!</h2>
         <a href="https://wa.link/njhw20" target="_blank">
           <button>CONFIRMAR ASISTENCIA</button>
         </a>
